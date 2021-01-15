a1e3bdfb46610aec8d7c339a8eb1bf2e2d89d5d1
mogaming <seiya.orz@gmail.com>
2021-01-15 16:39:09 +0900

    FIX: next config js for ci

--- src/next.config.js
+++ src/next.config.js
@@ -1,12 +1,16 @@
-const APP_ENV = process.env.APP_ENV || 'local'
-console.log("launch on", `node: ${process.env.NODE_ENV}`, `app_env: ${APP_ENV}`, `project: ${process.env.GCLOUD_PROJECT}`)
+let APP_ENV = process.env.APP_ENV
+console.log('launch on', `node: ${process.env.NODE_ENV}`, `app_env: ${APP_ENV}`)
 
 // 環境変数
-const envPath = `env/.env.${APP_ENV}`
-const fs = require('fs')
-fs.statSync(envPath) // envファイルの存在確認
-const DotEnv = require('dotenv-webpack')
-const envFile = new DotEnv({ path: envPath, systemvars: true })
+let envFile
+if (!APP_ENV) {
+  APP_ENV = 'local'
+  const envPath = `env/.env.${APP_ENV}`
+  const fs = require('fs')
+  fs.statSync(envPath) // envファイルの存在確認
+  const DotEnv = require('dotenv-webpack')
+  envFile = new DotEnv({ path: envPath, systemvars: true })
+}
 
 const nextConfig = {
   webpack: config => {
@@ -15,7 +19,9 @@ const nextConfig = {
 
     // env
     config.plugins = config.plugins || []
-    config.plugins.push(envFile)
+    if (envFile) {
+      config.plugins.push(envFile)
+    }
 
     return config
   },
