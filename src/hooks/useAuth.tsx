a55274cbf92f368ad224e49c73bde546bfbe86b4
mogaming <seiya.orz@gmail.com>
2021-01-15 16:02:36 +0900

    FIX: remove firebase

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, createContext, ReactNode, useContext } from 'react'
-import { auth } from 'lib/firebase/client'
-import firebase from 'firebase/app'
-import { User, Me } from 'model/user'
+import { User } from 'model/user'
 
 type UseAuthErrorCode = 'unexpected'
 
@@ -11,48 +9,10 @@ type AuthReturnType = {
   error?: UseAuthErrorCode
 }
 
-const convertUser = (user: firebase.User): User => {
-  return new Me(user.uid)
-}
-
 const _useAuth = (): AuthReturnType => {
   const [authState, setAuth] = useState<AuthReturnType>({ loading: true })
   useEffect(() => {
-    let cancel = false
-
-    const unsubscribe = auth.onAuthStateChanged(
-      async user => {
-        if (cancel) return
-        if (user) {
-          setAuth({ loading: false, user: convertUser(user) })
-          return
-        }
-
-        try {
-          const credential = await auth.signInAnonymously()
-          if (credential.user) {
-            setAuth({ loading: false, user: convertUser(credential.user) })
-            return
-          }
-
-          setAuth({ loading: false })
-        } catch (error) {
-          console.error(error)
-          setAuth({ loading: false, error: 'unexpected' })
-        }
-      },
-      error => {
-        if (cancel) return
-        console.error(error)
-        // FIXME: 適切なエラーに変換
-        setAuth({ loading: false, error: 'unexpected' })
-      }
-    )
-
-    return () => {
-      cancel = true
-      unsubscribe()
-    }
+    setAuth({ loading: false })
   }, [])
   return authState
 }
